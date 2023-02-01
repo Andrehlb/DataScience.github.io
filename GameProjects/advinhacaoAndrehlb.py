59e9deb250038786459acd95f28cdbae28b1ac4a
Andrehlb (Andre Luiz Barbosa) <andrehlbarbosa@gmail.com>
2023-02-01 20:06:41 -0300

    modified:   GameProjects/advinhacaoAndrehlb.py

--- GameProjects/advinhacaoAndrehlb.py
+++ GameProjects/advinhacaoAndrehlb.py
@@ -4,25 +4,28 @@ print("*********************************")
 
 numero_secreto = 42
 total_de_tentativas = 3
-while(total_de_tentativas > 0):
+tentativa = 1
+#while (total_de_tentativas > 0):
+while (total_de_tentativas >= tentativa):
+    #print("Tentativa", tentativa, "de", total_de_tentativas)
+    print("Tentativa {} de {}".format(tentativa, total_de_tentativas))
     chute_str = input("Digite teu número: ")
     print("Você digitiou: ", chute_str)
     chute = int(chute_str)
-
-"""chute_str = input("Digite o seu número: ")"""
-print("Você digitou: ", chute_str)
-chute = int(chute_str)
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
-if (acertou):
-    print("Parabéns, você acertou!")
-else:
-    if(maior):
-        print("Chute errado! Foi maior que o número secreto.")
-    elif(menor):
-        print("Chute errado! Foi menor que o número secreto.")
-print("Fun de Jogo! | O tipo de variável de chute é: ", type(chute))
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+    if (acertou):
+        print("Parabéns, você acertou!")
+    else:
+        if(maior):
+            print("Chute errado! Foi maior que o número secreto.")
+        elif(menor):
+            print("Chute errado! Foi menor que o número secreto.")
+#total_de_tentativas = total_de_tentativas -1
+tentativa = tentativa + 1
+print("Fim de Jogo!")
+print("O tipo de variável de chute é: ", type(chute))
 
 
 """
@@ -37,4 +40,4 @@ else:
     elif(idade > 12):
         print("Você é um adolescente.") 
 """
-        
+ 
